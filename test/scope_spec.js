806fad9710c1826483082fe9cfc0a9657265c6d9
Tomás Hugo <tomas.hugo@gmail.com>
2023-08-14 22:13:59 -0300

    test: ensure that watchCollection does not thrown on NaNs in arrays

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1391,5 +1391,20 @@ describe('Scope', function() {
             scope.$digest();
             expect(scope.counter).toBe(2);
         });
+
+        it('does not fail on NaNs in arrays', function() {
+            scope.arr = [2, NaN, 3];
+            scope.counter = 0;
+
+            scope.$watchCollection(
+                function(scope) { return scope.arr; },
+                function(newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
     });
 });
