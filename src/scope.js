f69445cd29f6f62a6fb71f4888b7206ffeea0605
Tomás Hugo <tomas.hugo@gmail.com>
2023-08-14 22:13:50 -0300

    feat: ensure that watchCollection does not thrown on NaNs in arrays

--- src/scope.js
+++ src/scope.js
@@ -259,7 +259,8 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
                 }
 
                 _.forEach(newValue, function(newItem, i) {
-                    if (newItem !== oldValue[i]) {
+                    var bothNaN = _.isNaN(newItem) && _.isNaN(oldValue[i]);
+                    if (!bothNaN && newItem !== oldValue[i]) {
                         changeCount++;
                         oldValue[i] = newItem;
                     }
