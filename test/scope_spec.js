2ebb429ceb1145a109b23af222057cf9b43a1a98
Tomás Hugo <tomas.hugo@gmail.com>
2023-09-05 08:20:35 -0300

    test: implement preventDefault on  and  page 175

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1794,6 +1794,17 @@ describe('Events', function () {
 
       expect(nextListener).toHaveBeenCalled();
     });
+
+    it('is sets defaultPrevented when preventDefault called on ' + method, function () {
+      var listener = function (event) {
+        event.preventDefault();
+      };
+      scope.$on('someEvent', listener);
+
+      var event = scope[method]('someEvent');
+
+      expect(event.defaultPrevented).toBe(true);
+    });
   });
 
   it('propagates up the scope hierarchy on $emit', function () {
