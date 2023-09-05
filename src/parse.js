7f8f2607bb648171ac1a6fd0347a27a4bf84b35a
Tomás Hugo <tomas.hugo@gmail.com>
2023-09-05 16:52:37 -0300

    feat: can parse scientific notations

--- src/parse.js
+++ src/parse.js
@@ -4,19 +4,37 @@ function Lexer () {
 
 }
 
+// Init Utilities
+
 Lexer.prototype.peek = function () {
   return this.index < this.text.length - 1 ? this.text.charAt(this.index + 1) : false;
 }
 
+Lexer.prototype.isExpOperator = function (ch) {
+  return ch === '-' || ch === '+' || this.isNumber(ch);
+}
+
+// End Utilities
+
 Lexer.prototype.readNumber = function () {
   var number = '';
   while (this.index < this.text.length) {
-    var ch = this.text.charAt(this.index);
+    var ch = this.text.charAt(this.index).toLowerCase();
     if (this.isNumber(ch) || ch === '.') {
       number += ch;
     }
     else {
-      break;
+      var nextCh = this.peek();
+      var prevCh = number.charAt(number.length - 1);
+      if (ch === 'e' && this.isExpOperator(nextCh)) {
+        number += ch;
+      } else if (this.isExpOperator(ch) && prevCh === 'e' && nextCh && this.isNumber(nextCh)) {
+        number += ch;
+      } else if (this.isExpOperator(ch) && prevCh === 'e' && (!nextCh || !this.isNumber(nextCh))) {
+        throw 'Invalid exponent';
+      } else {
+        break;
+      }
     }
     this.index++;
   }
